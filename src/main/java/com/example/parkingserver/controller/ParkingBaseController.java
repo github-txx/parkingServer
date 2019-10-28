5db86ac80015acf222963366bc5575fa6df15fd6
zhousz <zhouszxfs@xinfushe.com>
2019-10-28 15:22:33 +0800

    基础类

---
+++ src/main/java/com/example/parkingserver/controller/ParkingBaseController.java
@@ -0,0 +1,12 @@
+package com.example.parkingserver.controller;
+
+/**
+ * 基础控制类
+ *
+ * @author zhouszxfs@xinfushe.com
+ * @date 2019-10-28 15:22
+ * @param
+ * @return
+ */
+public class ParkingBaseController {
+}
