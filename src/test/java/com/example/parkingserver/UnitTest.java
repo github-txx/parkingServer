66655135d295127410151af6618345ff2b26729f
zhousz <zhouszxfs@xinfushe.com>
2019-10-29 14:35:15 +0800

    单元测试

---
+++ src/test/java/com/example/parkingserver/UnitTest.java
@@ -0,0 +1,20 @@
+package com.example.parkingserver;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 单元测试
+ *
+ * @author zhouszxfs@xinfushe.com
+ * @date 2019-10-29 14:34
+ * @param
+ * @return
+ */
+public class UnitTest {
+
+    @Test
+    public void main() {
+
+    }
+
+}
